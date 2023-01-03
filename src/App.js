cfd191dabfcb81ba92be7119e3712ae0c3d93f23
26haroon26 <26haroon26@gmail.com>
2023-01-03 23:16:03 +0500

    cccc

--- src/App.js
+++ src/App.js
@@ -34,7 +34,6 @@ function App() {
       try {
         let response = await axios.get(
           `${state.baseUrl}/profile`,
-          {},
           {
             withCredentials: true,
           }
